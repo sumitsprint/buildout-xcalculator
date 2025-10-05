ee87d89875c37dbe9e1fcc899fa27bf323e04cb6
Sumit Kumar. <ncr2099@gmail.com>
2025-10-05 13:24:28 +0530

    Calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,75 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { column } from 'mathjs';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [expr, setExpr] = useState("");
+  const [ans, setAns] = useState("");
 
+  const Buttons = ["7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "*", "C", "0", "=", "/"];
+  const simpleEvaluate = (e) => {
+    return Function(`return (${e})`)();
+
+  }
+const handleClick = (key) => {
+  if(key === "C"){
+    setExpr("");
+    setAns("");
+    return;
+  }
+  else if(key === "="){
+   if(!expr){
+     setAns("Error");
+  }
+
+   const res = simpleEvaluate(expr);
+   setAns(String(res));
+   return;
+  }
+  else {
+    setExpr(expr.concat(key));
+  }
+  
+
+}
+ 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <div className='App' style={{width: "500px"}}>
+
+          <h1 style={{textAlign: 'center'}}>React Calculator</h1>
+          <div style={{textAlign: 'center'}}>
+            <input
+            name = "input"
+            value = {expr}
+            type = "text"
+           readOnly
+            />
+          </div>
+          <br/>
+          <div style={{textAlign: 'center'}}>
+           {ans}
+          </div>
+          <br/>
+          <br/>
+      <div
+  style={{
+    display: 'grid', 
+    gap: '5px',
+    gridTemplateColumns: 'repeat(4, 1fr)',
+    textAlign: 'center'
+  }}
+>
+  {Buttons.map((btn) => (
+    <button key={btn} onClick={() => handleClick(btn)}>{btn}</button>
+  ))}
+</div>
+
+          
+        </div>
+        </div>
   )
 }
 
